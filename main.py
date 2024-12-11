65fc403bb78ea8db656d8ac3a613dbf5c3f7f412
Annie0523 <anyiw887@gmail.com>
2024-12-11 14:09:39 -0800

    student.py done

--- main.py
+++ main.py
@@ -24,10 +24,9 @@ from api.group import group_api
 from api.section import section_api
 from api.nestPost import nestPost_api # Justin added this, custom format for his website
 from api.messages_api import messages_api # Adi added this, messages for his website
-from api.carphoto import car_api
-from api.carChat import car_chat_api
-
 from api.vote import vote_api
+from student import student_api # Anyi added
+
 # database Initialization functions
 from model.carChat import CarChat
 from model.user import User, initUsers
@@ -47,12 +46,11 @@ app.register_blueprint(post_api)
 app.register_blueprint(channel_api)
 app.register_blueprint(group_api)
 app.register_blueprint(section_api)
-app.register_blueprint(car_chat_api)
+app.register_blueprint(student_api)
 # Added new files to create nestPosts, uses a different format than Mortensen and didn't want to touch his junk
 app.register_blueprint(nestPost_api)
 app.register_blueprint(nestImg_api)
 app.register_blueprint(vote_api)
-app.register_blueprint(car_api)
 
 # Tell Flask-Login the view function name of your login route
 login_manager.login_view = "login"
---
+++ student.py
@@ -0,0 +1,85 @@
+from flask import Blueprint, jsonify
+from flask_restful import Api, Resource # used for REST API building
+
+student_api = Blueprint('student_api', __name__,
+                   url_prefix='/api')
+
+# API docs https://flask-restful.readthedocs.io/en/latest/
+api = Api(student_api)
+
+class StudentAPI:        
+    class _Johan(Resource): 
+        def get(self):
+           # implement the get method 
+           data =  {
+                "FirstName": "Johan",
+                "LastName": "Mascarenhas",
+                "DOB": "Jnauary 8",
+                "Residence": "San Diego",
+                "Email": "johanmascarenhas.jm@gmail.com",
+                "Favorite_song": "Thick of it by KSI",
+                "Hobbies": ["Gymnasium", "swimming", "Videogames"]
+            }
+           return jsonify(data)
+    
+    class _Luke(Resource): 
+        def get(self):
+           # implement the get method 
+           data =  {
+                "FirstName": "Luke",
+                "LastName": "Starr",
+                "DOB": "November 2",
+                "Residence": "San Diego",
+                "Email": "lstarr1100@gmail.com",
+                "Favorite_Videogame": "Elden Ring",
+                "Hobbies": ["Videogames", "Guitar", "Musicproduction"]
+            }
+           return jsonify(data)
+       
+    class _Anyi(Resource): 
+        def get(self):
+           # implement the get method 
+           data =  {
+                "FirstName": "Anyi",
+                "LastName": "Wang",
+                "DOB": "May 23",
+                "Residence": "San Diego",
+                "Email": "anyiw887@gmail.com",
+                "Favorite_Videogame": "Splatoon",
+                "Hobbies": ["Baking", "Chinese Zither", "Badminton"]
+            }
+           return jsonify(data)
+    
+    class _Collin(Resource): 
+        def get(self):
+           # implement the get method 
+           data =  {
+                "FirstName": "Collin",
+                "LastName": "Ge",
+                "DOB": "March 26",
+                "Residence": "San Diego",
+                "Email": "collinxiaoheizi@gmail.com",
+                "Favorite_Videogame": "Honkai starrail",
+                "Hobbies": ["Cooking", "fishing", "crafting stuff"]
+            }
+           return jsonify(data)
+       
+    class _Michelle(Resource): 
+        def get(self):
+           data = {
+               "FirstName": "Michelle",
+                "LastName": "Kuruvilla",
+                "DOB": "July 7",
+                "Residence": "San Diego",
+                "Email": "kuruvillamichelle@gmail.com",
+                "Favorite_Videogame": "Agario",
+                "Hobbies": ["Eating", "Listeningtomusic", "Reading"]
+           }
+           return jsonify(data)
+
+    # building RESTapi endpoint
+    api.add_resource(_Johan, '/student/johan')          
+    api.add_resource(_Luke, '/student/luke')
+    api.add_resource(_Anyi, '/student/anyi')
+    api.add_resource(_Collin, '/student/collin')
+    api.add_resource(_Michelle, '/student/michelle')
