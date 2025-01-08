3abb5c65222b8e96addcfc32358c904554be529e
collin12396 <collinxiaoheizi@gmail.com>
2025-01-08 15:05:37 -0800

    fix comment

--- api/comments.py
+++ api/comments.py
@@ -1,12 +1,6 @@
-import jwt
-from flask import Blueprint, request, jsonify, current_app, Response, g
+from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource  # used for REST API building
-from datetime import datetime
 from __init__ import app
-from api.jwt_authorize import token_required
-from model.channel import Channel
-from model.group import Group
-from model.user import User
 
 
 comment_api = Blueprint("comment_api", __name__, url_prefix='/api')
