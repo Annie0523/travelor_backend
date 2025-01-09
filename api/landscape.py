a01fafe39273fb599b2df33a63deb96f819980cf
Annie0523 <anyiw887@gmail.com>
2025-01-09 13:42:32 -0800

    add more info to landscape

--- api/landscape.py
+++ api/landscape.py
@@ -19,6 +19,11 @@ class LandscapeAPI:
                 "name": "Death Valley",
                 "location": "United States",
                 "Introduction": "Hottest place in the United States",
+            },  
+            "Zhangjiajie": {
+                "name": "Zhangjiajie",
+                "location": "China",
+                "Introduction": "Natural beauty of the mountains",
             },            
         }
         return landscapes.get(name)
@@ -35,10 +40,17 @@ class LandscapeAPI:
             if landscape:
                 return jsonify(landscape) 
             return {"Data not found"}, 404 
+    class _zhangjiajie(Resource): 
+        def get(self):
+            landscape = LandscapeAPI.get_landscape("Zhangjiajie")
+            if landscape:
+                return jsonify(landscape) 
+            return {"Data not found"}, 404 
 
     # building RESTapi endpoint
 api.add_resource(LandscapeAPI._greatwall, '/landscape/greatwall')
 api.add_resource(LandscapeAPI._deathvally, '/landscape/deathvalley')
+api.add_resource(LandscapeAPI._zhangjiajie, '/landscape/zhangjiajie')
 
 # Instantiate the StudentAPI to register the endpoints
 landscape_api_instance = LandscapeAPI()
