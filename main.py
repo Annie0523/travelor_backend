2ce0edd9ba892d30bd9a1104b118f671f0afe3d8
Annie0523 <anyiw887@gmail.com>
2025-01-09 13:29:24 -0800

    someone delete my stuff and I add it back :D

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ from api.nestPost import nestPost_api # Justin added this, custom format for his
 from api.messages_api import messages_api # Adi added this, messages for his website
 from api.vote import vote_api
 from api.student import student_api # Anyi added
+from api.landscape import landscape_api
 
 # database Initialization functions
 from model.carChat import CarChat
@@ -50,6 +51,7 @@ app.register_blueprint(channel_api)
 app.register_blueprint(group_api)
 app.register_blueprint(section_api)
 app.register_blueprint(student_api)
+app.register_blueprint(landscape_api)
 # Added new files to create nestPosts, uses a different format than Mortensen and didn't want to touch his junk
 app.register_blueprint(nestPost_api)
 app.register_blueprint(nestImg_api)
